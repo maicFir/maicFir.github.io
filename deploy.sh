5c1b20ba69f846439dee49ac1724c27f9d699822
maicFir <anne.duan@aax.com>
2022-07-21 21:41:48 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,18 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+# 生成静态文件
+npm run build 
+
+# 进入生成的文件夹
+cd docs/.vuepress/dist
+
 git status
 
-git add .
-
-git commit -m '新增自动化脚本'
-
-git push origin dev2.0
+git init
+git add -A
+git commit -m 'deploy'
+# 推送到指定仓库
+git push -f https://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@github.com/maicFir/maicFir.github.io.git master
 
 echo 'push success'
