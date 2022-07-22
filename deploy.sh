2184caf2879048ca025e533d38195e2d921c2b94
maicFir <mcodes@163.com>
2022-07-22 11:27:37 +0800

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
  # @Author: maicFir mcodes@163.com
  # @Date: 2022-07-21 21:36:50
  # @LastEditors: maicFir mcodes@163.com
- # @LastEditTime: 2022-07-22 11:17:44
+ # @LastEditTime: 2022-07-22 11:26:37
  # @FilePath: /maicFir.github.io/deploy.sh
  # @Description: 
 ### 
@@ -21,7 +21,7 @@ git init
 git add .
 git commit -m 'deploy'
 # 推送到指定仓库
-git remote add origin https://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@github.com/maicFir/maicFir.github.io.git
+git remote add origin https://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@github.com/maicFir/maicFir.github.io.git
 
 git push origin master
 
