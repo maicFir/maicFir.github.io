4d31c2e0a870ffca5927edd3f65fbe1bfd92b9ec
maicFir <mcodes@163.com>
2022-08-17 12:05:19 +0800

    fix: 去掉鼠标点击效果

--- docs/.vuepress/components/Sparkler/view/Index.vue
+++ docs/.vuepress/components/Sparkler/view/Index.vue
@@ -3,38 +3,38 @@
 </template>
 
 <script>
-import { defineComponent, onMounted, onUnmounted, ref } from "vue";
-import { Popper, PopperShape, MAX_Z_INDEX } from "@moefy-canvas/theme-popper";
-const weakMap = new WeakMap();
+// import { defineComponent, onMounted, onUnmounted, ref } from "vue";
+// import { Popper, PopperShape, MAX_Z_INDEX } from "@moefy-canvas/theme-popper";
+// const weakMap = new WeakMap();
 
-export default defineComponent({
-  name: "Spakler",
-  setup(props, ctx) {
-    const themeConfig = {
-      shape: PopperShape.Star,
-      size: 1.75,
-      numParticles: 10,
-    };
-    const canvasOptions = {
-      opacity: 1,
-      zIndex: MAX_Z_INDEX,
-    };
-    const canvansEl = ref(null);
-    const sparkler = new Popper(themeConfig, canvasOptions);
-    const installSpakler = () => {
-      const curentCanvansDom = document.getElementById("canvas");
-      canvansEl.value = curentCanvansDom;
-      weakMap.set(curentCanvansDom, curentCanvansDom);
-      sparkler.mount(weakMap.get(curentCanvansDom));
-    };
-    onMounted(() => {
-      // 点击鼠标烟花效果
-      installSpakler();
-    });
-    onUnmounted(() => {
-      sparkler.unmount(weakMap.get(canvansEl.value));
-    });
-    return {};
-  },
-});
+// export default defineComponent({
+//   name: "Spakler",
+//   setup(props, ctx) {
+//     const themeConfig = {
+//       shape: PopperShape.Star,
+//       size: 1.75,
+//       numParticles: 10,
+//     };
+//     const canvasOptions = {
+//       opacity: 1,
+//       zIndex: MAX_Z_INDEX,
+//     };
+//     const canvansEl = ref(null);
+//     const sparkler = new Popper(themeConfig, canvasOptions);
+//     const installSpakler = () => {
+//       const curentCanvansDom = document.getElementById("canvas");
+//       canvansEl.value = curentCanvansDom;
+//       weakMap.set(curentCanvansDom, curentCanvansDom);
+//       sparkler.mount(weakMap.get(curentCanvansDom));
+//     };
+//     onMounted(() => {
+//       // 点击鼠标烟花效果
+//       installSpakler(); // 主要是@moefy-canvas/theme-popper这个包报load ES Moudle错误
+//     });
+//     onUnmounted(() => {
+//       sparkler.unmount(weakMap.get(canvansEl.value));
+//     });
+//     return {};
+//   },
+// });
 </script>
