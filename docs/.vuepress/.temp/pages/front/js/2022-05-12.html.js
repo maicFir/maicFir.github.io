1e88bdb7e294d118b3a4569fff935bc04195218f
maicFir <mcodes@163.com>
2022-08-17 11:17:59 +0800

    test 自动化服务器部署

--- docs/.vuepress/.temp/pages/front/js/2022-05-12.html.js
+++ docs/.vuepress/.temp/pages/front/js/2022-05-12.html.js
@@ -1,14 +1,1 @@
 export const data = JSON.parse("{\"key\":\"v-25f5bed0\",\"path\":\"/front/js/2022-05-12.html\",\"title\":\"掌握闭包，夯实基本功\",\"lang\":\"zh-CN\",\"frontmatter\":{\"title\":\"掌握闭包，夯实基本功\",\"date\":\"2022-05-12T00:00:00.000Z\",\"sidebarDepth\":3},\"excerpt\":\"\",\"headers\":[{\"level\":3,\"title\":\"闭包是什么\",\"slug\":\"闭包是什么\",\"children\":[]},{\"level\":3,\"title\":\"闭包的特性\",\"slug\":\"闭包的特性\",\"children\":[]},{\"level\":3,\"title\":\"应用场景\",\"slug\":\"应用场景\",\"children\":[]},{\"level\":3,\"title\":\"总结\",\"slug\":\"总结\",\"children\":[]}],\"git\":{\"updatedTime\":1658316307000,\"contributors\":[{\"name\":\"maicFir\",\"email\":\"anne.duan@aax.com\",\"commits\":1}]},\"filePathRelative\":\"front/js/2022-05-12.md\"}")
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
